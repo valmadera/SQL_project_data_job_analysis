5b33e00707ded997fd64659c0f344ba56d1a71c8
valmadera <valmaderac@gmail.com>
2024-05-02 13:36:15 -0400

    push example

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -13,13 +13,13 @@ NOTE: If you are having issues with permissions. And you get error:
     1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
-\copy company_dim FROM 'C:\Users\User01\Documents\TRABAJO\Postulaciones de empleo\Data Analyst\SQL_project_data_job_analysis\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy company_dim FROM 'csv file path company_dim' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_dim FROM 'C:\Users\User01\Documents\TRABAJO\Postulaciones de empleo\Data Analyst\SQL_project_data_job_analysis\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_dim FROM 'cvs file path skills_dim' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy job_postings_fact FROM 'C:\Users\User01\Documents\TRABAJO\Postulaciones de empleo\Data Analyst\SQL_project_data_job_analysis\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy job_postings_fact FROM 'cvs file path job_postings_fact' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_job_dim FROM 'C:\Users\User01\Documents\TRABAJO\Postulaciones de empleo\Data Analyst\SQL_project_data_job_analysis\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_job_dim FROM 'cvs file path skills_job_dim' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
 
