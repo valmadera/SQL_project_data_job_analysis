1b38b8378826144072e2b62048a227f3313f8050
valmadera <valmaderac@gmail.com>
2024-05-03 12:29:01 -0400

    commit

--- sql_job_project/1_top_paying_jobs.sql
+++ sql_job_project/1_top_paying_jobs.sql
@@ -1,13 +1,6 @@
--- IMPORTANT
-/*UPDATE job_postings_fact
-SET salary_year_avg = 65000
-WHERE 
-    job_id = 226942 AND
-    salary_year_avg = 650000*/
-
-/* What are the top-paying jobs?
+/* What are the top-paying remote jobs?
 - Only include jobs with salaries.
-- Identifing the top 10 hihgest paying jobs available remotely.
+- Identifing the top 10 hihgest paying jobs available anywhere.
 - Identify the companies that offer this jobs.
 - Identify the platforms where I can apply for these roles.
 - Why? This will provide insights about roles I can apply aiming to get the highest payment. Also
@@ -15,28 +8,19 @@ I will be able to see the companies that offer the best salaries and where I can
 job.
 */
 
-WITH job_application AS (
-    SELECT *
-    FROM
-        job_postings_fact 
-        INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-    WHERE
-        salary_year_avg IS NOT NULL
-    )
-
 SELECT
     job_title,
     name,
     job_country,
     salary_year_avg,
     job_via
-
 FROM
-    job_application
-
+    job_postings_fact 
+    INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_location = 'Anywhere' 
-
-ORDER BY salary_year_avg DESC
-
-LIMIT 10;
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 
+    10;
--- sql_job_project/2_top_paying_skills.sql
+++ sql_job_project/2_top_paying_skills.sql
@@ -1,25 +1,38 @@
-/* What are the top-paying skills?
-- Select the skills for the previous top 10 jobs.
-- Add the specific skills required for these jobs.
+/* What are the top skills?
+- Select the top 5 in-demand skills and their asociate estimated average salary.
 - Why? This will provide with a list of skills that I can learn, with the aim of increasing
-my possibilities of getting the job possition. 
+my possibilities of getting a job position. 
 */
+
+WITH job_skills AS (
+    SELECT 
+        job_postings_fact.job_id,
+        job_title,
+        skills,
+        salary_year_avg
+    FROM
+        job_postings_fact 
+        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+        INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        salary_year_avg IS NOT NULL AND
+        job_location = 'Anywhere'
+    ORDER BY 
+        salary_year_avg DESC
+    )
+
 
 SELECT
     skills,
     COUNT (skills) AS count_of_requested_skill,
     ROUND (AVG (salary_year_avg),0) AS avg_salary
-
 FROM
-    job_postings_fact 
-    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    job_skills
+GROUP BY 
+    skills
+ORDER BY 
+    count_of_requested_skill DESC
+LIMIT
+     5;
 
-WHERE
-    salary_year_avg IS NOT NULL
 
-GROUP BY skills
-
-ORDER BY count_of_requested_skill DESC
-
-LIMIT 5;
